ae08473cdea6ae9a8b0ccba2067bc552ce402be2
komal kd <komaldedha2001+personal@gmail.com>
2023-09-20 20:03:27 +0530

    add initial file

---
+++ komal.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("KD");
+    printf("20 sep 2023");
+    return 0;
+}
---
+++ komal.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"komal dedha gurjar";
+    cout<<"20 sep 2023";
+    return 0;
+}
